0a89271289d7f0653679520528b113bb32f2e37f
Slavisa05 <slavisaarsenijevic05@gmail.com>
2026-02-11 12:22:03 +0100

    Zavrsio ceo dashboard

--- salons/static/salons/js/salons.js
+++ salons/static/salons/js/salons.js
@@ -1,1 +1,10 @@
 console.log('salons.js');
+
+const sidebarToggle = document.getElementById('sidebarToggle');
+
+if (sidebarToggle) {
+	sidebarToggle.addEventListener('click', () => {
+		const isCollapsed = document.body.classList.toggle('sidebar-collapsed');
+		sidebarToggle.setAttribute('aria-expanded', String(!isCollapsed));
+	});
+}
