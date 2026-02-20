f1f57a0017c7f8d231077c3d3d49242b1eb294e9
Slavisa05 <slavisaarsenijevic05@gmail.com>
2026-02-20 14:26:12 +0100

    presao na postgreSQL

--- sistem_zakazivanja/settings.py
+++ sistem_zakazivanja/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from decouple import config
 
 load_dotenv()
 
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'sistem_zakazivanja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME', default='salon_app_db'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
