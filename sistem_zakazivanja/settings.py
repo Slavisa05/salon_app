5bbee845fddc7e12158f9ad3b5fe3a2326e795e4
Slavisa <slavisaarsenijevic05@gmail.com>
2026-05-26 18:29:03 +0200

    mala promena u settings

--- sistem_zakazivanja/settings.py
+++ sistem_zakazivanja/settings.py
@@ -173,3 +173,4 @@ CELERY_BEAT_SCHEDULE = {
 FORCE_SCRIPT_NAME = "/salon"
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ["https://slavisadev.com", "https://www.slavisadev.com"]
